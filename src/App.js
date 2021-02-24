36e8e50b620f5e74f8eb16b91e6fef9395bf54ef
safiratrisa <trisa.tsh@gmail.com>
2021-02-24 14:56:47 +0700

    getGithubAPIconsole

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
+class App extends Component {
+  componentDidMount () {
+    this.checkUsername()
+  }
+  checkUsername = () => {
+    axios.get(`http://api.github.com/users/safiratrisa`)
+    .then(res => {
+        console.log(res.data)
+    })
+    .catch(err => {
+        console.log(err.response)
+    })
+  }
+  render() {   
+    return (
+      <div className="App">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
