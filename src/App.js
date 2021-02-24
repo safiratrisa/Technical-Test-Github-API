a1c7f6a59b073005951f866b45b203f1ddf2b68f
safiratrisa <trisa.tsh@gmail.com>
2021-02-24 15:30:24 +0700

    GithubAPI Input+Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 class App extends Component {
-  componentDidMount () {
-    this.checkUsername()
+  constructor(props) {
+    super(props);
+    this.checkUsername = this.checkUsername.bind(this);
   }
   checkUsername = () => {
-    axios.get(`http://api.github.com/users/safiratrisa`)
+    const username = this.input.value
+    axios.get(`http://api.github.com/users/${username}`)
     .then(res => {
         console.log(res.data)
     })
@@ -15,8 +17,12 @@ class App extends Component {
   }
   render() {   
     return (
-      <div className="App">
-
+      <div>
+        <input
+          type="text"
+          ref={inputUsername => (this.input = inputUsername)}
+        />
+        <button onClick={this.checkUsername}>Check</button>
       </div>
     );
   }
