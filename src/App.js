a71cc3d5ba2fb32ce02ff9878ccbf243a7ef6ef3
safiratrisa <trisa.tsh@gmail.com>
2021-02-24 16:03:44 +0700

    username exists or not

--- src/App.js
+++ src/App.js
@@ -4,25 +4,46 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.checkUsername = this.checkUsername.bind(this);
+    this.state = {
+      user: '',
+      error: ''
+    }
   }
   checkUsername = () => {
     const username = this.input.value
     axios.get(`http://api.github.com/users/${username}`)
     .then(res => {
-        console.log(res.data)
+      this.setState({
+        error: '',
+        user: res.data
+      });
     })
     .catch(err => {
-        console.log(err.response)
+      this.setState({
+        user: '',
+        error: err.response.data.message
+      });
     })
   }
-  render() {   
+  render() { 
+    const error = this.state.error
+    const name = this.state.user.name
+    let userProfile;
+    if (name) {
+      userProfile = <h1>Username Exists</h1>
+    } else if (error) {
+      userProfile = <h1>Username Does Not Exist</h1>
+    }
     return (
       <div>
+        <div>
         <input
           type="text"
           ref={inputUsername => (this.input = inputUsername)}
         />
         <button onClick={this.checkUsername}>Check</button>
+        </div>
+        {userProfile}
       </div>
     );
   }
